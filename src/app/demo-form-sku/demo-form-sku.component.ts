80a4992666267f0751d59802f2fc025dabe56ccc
Landver <good1ni@mail.ru>
2018-08-20 00:32:16 +0300

    160

--- src/app/demo-form-sku/demo-form-sku.component.ts
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-form-sku',
@@ -11,8 +11,14 @@ export class DemoFormSkuWithBuilderComponent implements OnInit {
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      'sku': ['ABC123']
+      'sku': ['', Validators.required]
     });
+  }
+
+  skuValidator(control: FormControl): { [s: string]: boolean } {
+    if (!control.value.match(/^123/)) {
+      return {invalidSku: true};
+    }
   }
 
   ngOnInit() {
